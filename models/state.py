502f82a00c4e3fdb6a0e1b2d5855b07b9d3eb75d
AhmedSayed210 <romaahmed791@gmail.com>
2024-02-21 22:39:48 +0200

    task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+from models.city import City
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    # For DBStorage
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    # For FileStorage
+    else:
+        @property
+        def cities(self):
+            """Getter attribute cities"""
+            from models import storage
+            cities_list = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
+
