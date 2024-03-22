e459e51354ba0566e9ab136cf66099e0652cb6f2
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-03-22 18:36:31 -0400

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+""" script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,4 +13,4 @@ def home():
 
 
 if __name__ == '__main__':
-    run.app(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
