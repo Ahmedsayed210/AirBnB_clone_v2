46caa5e85e21d5dc776ecebe8c414890d521fc98
AhmedSayed210 <romaahmed791@gmail.com>
2024-03-23 21:46:36 +0200

    4-task

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -48,7 +48,7 @@ def python(text="is cool"):
 
 @app.route('/number/<n>', strict_slashes=False)
 def number(n):
-    return f"{n}is a number"
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
